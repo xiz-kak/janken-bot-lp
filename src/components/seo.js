055f47e3c73aa7e9b2b039a33081a5a147c3a6b0
xiz-tky <shizuka.kakimoto@linkage.jp.net>
2021-04-21 00:26:45 +0900

    add og:image

--- src/components/seo.js
+++ src/components/seo.js
@@ -9,6 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
+import ogpImage from "../../static/images/logo_mark.png"
 
 function SEO({ description, lang, meta, title }) {
   const { site } = useStaticQuery(
@@ -42,6 +43,10 @@ function SEO({ description, lang, meta, title }) {
         {
           property: `og:title`,
           content: title,
+        },
+        {
+          property: `og:image`,
+          content: ogpImage,
         },
         {
           property: `og:description`,
